3bc37ce40165a833c634f0f0fcd5b11e33d23a1c
vanshavenger <vanshchopra101@gmail.com>
2024-12-15 03:08:21 +0530

    Initial commit

---
+++ test/efficint-upload.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EfficintUpload from '../lib/efficint-upload-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/efficint-upload-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EfficintUpload.EfficintUploadStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
